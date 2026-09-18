550186ffe1fa18db87011f4c273a677c0dab392e
sDANTOSO <winatsa@whs.school.nz>
2026-09-19 01:45:26 +1200

    added tower scene

---
+++ apocalypse-monkeyy/Scripts/tower.gd
@@ -0,0 +1,80 @@
+extends Node2D
+## what if there was a tower scene where the final battle was and its either like they raise up a level of the tower each time
+## or its like your going up the tower and you can see like the whole tower so you can predict whats happening through walls
+## have combos of jumping out windows and stuff like that etc
+
+
+## how do we make this game dynamic
+## i think understanding and constantly changing and surprising the user through how the monkey can move through the game would be cool
+## like theres games where time is paused and your exploring a scene
+## or theres action games where your trying to find the action that will save you or help you escape
+
+## to me a text game is just a game where you pause time
+
+## so this could just kinda be a kinda action/exploration game that is paused at dialogue moments or other important moments
+
+## so how could this go down
+##monke in spa
+## monke gets message abt apocalypse
+## monke tries to stop apocalypse
+
+##monke fails
+## but how do we make this dynamic and engaging
+## i think what makes dispatch engaging is because it offers 
+## great looking story
+## challenge when your the operator
+##some sense of choice when your choosing what to say
+
+## what will make this game engaging
+
+## some sense of choice when choosing what to say
+## challenging minigames
+## interactive world or dynamic world
+## what will make our world interactive
+##maybe making the world movable like it is in real life
+##adding 3d world parts in
+
+## what could cause an apocalypse 
+##climate change
+##nukes(probably bad for japan)
+##Ai (highkey overused)
+##war
+## what about climate change war
+## im thinking like climate change has caused only the billionares to have a secure food chain
+## a lot of the monkeys have lost their habitat and food 
+## the hit monkey is the last of his race or maybe one part of the last remaining animals
+## whereas the cowboy monkeys are being fed by the billionares to protect them 
+
+## what does this apocalypse have
+## slightly destroyed buildings
+## lack of food
+## dangerous weather
+
+## what would this monkey have to do
+## protect their tribe
+## track down the billionares
+## find food
+## survive dangerous weather and the cowboys
+
+## how should chapters work
+## it should be each chapter has multiple subtasks or large tasks in it
+## to reach a part in the story 
+
+## chapter 1
+##monke trying to stop apocalypse and failing (omg failed harvests)
+##chapter 2
+##being challenged by everything
+##chapter 3
+##revealing the cowboys
+##chapter 4
+##revealing the real villains
+## chapter 5
+## the hunt
+##chapter 6
+## battles
+
+## possible locations
+##airport (hella protected)
+##imperial village(secrets)
+##rich people houses(protected)
+## his village
