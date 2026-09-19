a7e68a52a384741c650c33301f9aebc8092b9ed4
Remy Moreau <squeakycleanfreshie@gmail.com>
2026-09-19 14:13:41 +1000

    visal novel structure works yayay

--- apocalypse-monkeyy/Scenes/node_2d.gd
+++ apocalypse-monkeyy/Scenes/node_2d.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var character_Test = %Character_Test
 @onready var dialog_ui = %"Dialoge UI"
+var dialog_index : int = 0
 
 const dialog_lines : Array[String] = [
 	"Assassin Monkey: I am an assasin, not a entertainer",
@@ -9,9 +10,15 @@ const dialog_lines : Array[String] = [
 ]
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	process_line(parse_line(dialog_lines[0]))
+func _ready():
+	dialog_index = 0
+	process_current_line()
 
+func _input(event):
+	if event.is_action_pressed("next_line"):
+		if dialog_index < len(dialog_lines) - 1:
+			dialog_index += 1
+			process_current_line()
 
 func parse_line(line: String) -> Dictionary:
 	var line_info = line.split(":")
@@ -21,6 +28,8 @@ func parse_line(line: String) -> Dictionary:
 		"dialog_line": line_info[1]
 	}
 
-func process_line(line_info: Dictionary) -> void:
+func process_current_line():
+	var line_info = parse_line(dialog_lines[dialog_index])
 	dialog_ui.speaker_name.text = line_info["speaker_name"]
 	dialog_ui.dialog_line.text = line_info["dialog_line"]
+	
