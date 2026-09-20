20b9a479942ea4f7c54fcda81973ab60058c8b0e
sDANTOSO <winatsa@whs.school.nz>
2026-09-20 22:51:53 +1200

    added trees to forest

--- apocalypse-monkeyy/Scripts/character_body_2d.gd
+++ apocalypse-monkeyy/Scripts/character_body_2d.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const jump_powe = JUMP_VELOCITY
+const acc = 50
+const friction = 70
+const gravity = 120
+const wall_jump_pushback = 100
+
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +18,15 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall_only and Input.is_action_just_pressed("right"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = -wall_jump_pushback
+		if is_on_wall_only and Input.is_action_just_pressed("left"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = wall_jump_pushback
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
