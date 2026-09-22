2322176ce21a4cd222314e8929605bf03d8f6957
Remy Moreau <squeakycleanfreshie@gmail.com>
2026-09-22 19:30:08 +1000

    chenged JSON format

--- apocalypse-monkeyy/Scripts/node_2d.gd
+++ apocalypse-monkeyy/Scripts/node_2d.gd
@@ -6,28 +6,10 @@ var dialog_index : int = 0
 var current_speaker : String = ""
 
 
-const dialog_lines : Array[String] = [
-	"Sato: It's been 6 months since the start",
-	"Sato: My garden. It hasn't even grown",
-	"Sato: There has been no fruit. No vegetables. No Income",
-	"Sato: No Harvest.",
-	"Sato: My family has started to starve",
-	"Sato: I must do something!",
-	"Sato: But what.",
-	"Sato: I know there is something not natural about this.",
-	"Sato: Ever since the ice age started, all the cities got empty",
-	"Sato: Everyone evacuated.",
-	"Sato: Or died.",
-	"Sato: In Japan the only people who roam the streets are criminals",
-	"Sato: Japan has become a wasteland",
-	"Sato: Its time for me to go.",
-	"Sato: I must find other people who want change.",
-	"Sato: Who want to stop the apocolypse.",
-	"Sato: And take down whatever, and whoever is cauing it."
-
-]
+var  dialog_lines : Array = []
 
 func _ready():
+	dialog_lines = load_dialog("res://Recourses/story/story.json")
 	dialog_ui.finished_typing.connect(_on_finished_typing)
 	dialog_index = 0
 	process_current_line()
@@ -37,23 +19,41 @@ func _input(event):
 		if dialog_index < len(dialog_lines) - 1:
 			dialog_index += 1
 			process_current_line()
-		if dialog_index==10:
+		if dialog_index == 10:
 			get_tree().change_scene_to_file("res://Scenes/forest.tscn")
 
-func parse_line(line: String) -> Dictionary:
-	var line_info = line.split(":")
-	assert(len(line_info) >= 2)
-	return {
-		"speaker_name": line_info[0],
-		"dialog_line": line_info[1]
-	}
-
 func process_current_line():
-	var line_info = parse_line(dialog_lines[dialog_index])
-	current_speaker = line_info["speaker_name"]
-	dialog_ui.set_line(line_info["speaker_name"], line_info["dialog_line"])
+	var line = dialog_lines[dialog_index]
+	current_speaker = line["speaker_name"]
+	dialog_ui.set_line(line["speaker_name"], line["dialog_line"])
 	character_Test.change_character(current_speaker, true)
-	
 
 func _on_finished_typing():
 	character_Test.change_character(current_speaker, false)
+
+func load_dialog(file_path):
+	# check if file exists
+	if not FileAccess.file_exists(file_path):
+		printerr("ERROR: File does not exist: ", file_path)
+		return null
+
+	# open the file
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		printerr("ERROR: Failed to open file: ", file_path)
+		return null
+
+	# read the content of the file
+	var content = file.get_as_text()
+
+	# parse the JSON
+	var json_content = JSON.parse_string(content)
+
+	# check if parsing was successful
+	if json_content == null:
+		printerr("ERROR: Failed to parse JSON: ", file_path)
+		return null
+
+	# return the dialog
+	return json_content
+	
